025f3125766241b28caced36bcee86741c96ca30
Adam Fousek <adamfousek@Adams-MacBook-Air.local>
2020-06-27 13:32:13 +0200

    new project

---
+++ adamfousek/todolist/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'todolist/index.html', {'pageName': 'Index'})
+
+
+def dashboard(request):
+    return render(request, 'todolist/dashboard.html', {'pageName': 'Dashboard'})

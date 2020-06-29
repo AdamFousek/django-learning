900eb0b9909a37611c17c6b416390fabd80bc4f0
Fousek <adam.fousek@okin.eu>
2020-06-29 11:50:55 +0200

    CBV tutorial

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import (
+    View,
+    TemplateView,
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from . import models
 
 
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("Class based views")
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class SchoolCreateView(CreateView):
+    model = models.School
+    fields = ('name', 'principal', 'location')
+
+
+class SchoolUpdateView(UpdateView):
+    model = models.School
+    fields = ('name', 'principal')
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
